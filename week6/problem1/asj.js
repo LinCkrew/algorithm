353fd70a15b8c4ebb58df72a9a94a98db94a8485
An Seungji <ansj8777@gmail.com>
2024-12-28 16:57:26 +0900

    안승지, 6주차 스택 적용 해답 참고 풀이, 추가 학습 후 복습 예정

--- week6/problem1/asj.js
+++ week6/problem1/asj.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const input = fs
   .readFileSync('./dev/stdin/index.txt')
-  //   .readFileSync('/dev/stdin')
+  // .readFileSync('/dev/stdin')
   .toString()
   .trim()
   .split('\n');
@@ -10,15 +10,23 @@ const N = Number(input.shift());
 const heights = input.map(Number);
 
 let count = 0;
+const stack = [];
 
-for (let i = 0; i < heights.length; i++) {
-  for (let j = i + 1; j < heights.length; j++) {
-    if (heights[i] < heights[j]) {
-      count += 1;
-      break;
-    } else {
-      count += 1;
-    }
+for (let i = 0; i < N; i++) {
+  // 현재 사람보다 키가 작은 사람들은 제거하면서 카운트
+  while (stack.length > 0 && stack[stack.length - 1][0] < heights[i]) {
+    count += stack.pop()[1];
+  }
+
+  // 같은 키를 가진 사람들 처리
+  if (stack.length > 0 && stack[stack.length - 1][0] === heights[i]) {
+    const [height, cnt] = stack.pop();
+    count += cnt;
+    if (stack.length > 0) count++;
+    stack.push([height, cnt + 1]);
+  } else {
+    if (stack.length > 0) count++;
+    stack.push([heights[i], 1]);
   }
 }
 
