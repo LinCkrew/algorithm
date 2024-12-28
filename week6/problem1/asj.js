f3b165ca3115fb76b228c5c9189d5aa3fe373ec8
An Seungji <ansj8777@gmail.com>
2024-12-28 16:46:31 +0900

    6주차, 스택 미적용 풀이, 오답

--- week6/problem1/asj.js
+++ week6/problem1/asj.js
@@ -5,3 +5,21 @@ const input = fs
   .toString()
   .trim()
   .split('\n');
+
+const N = Number(input.shift());
+const heights = input.map(Number);
+
+let count = 0;
+
+for (let i = 0; i < heights.length; i++) {
+  for (let j = i + 1; j < heights.length; j++) {
+    if (heights[i] < heights[j]) {
+      count += 1;
+      break;
+    } else {
+      count += 1;
+    }
+  }
+}
+
+console.log(count);
